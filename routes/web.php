6ccafcf43cd458a7633cfb3937b5ef83262213a2
Cam- <veillardcamille01@gmail.com>
2025-05-22 09:37:26 +0200

    upgrading the admin with Filament

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/admin', [DashboardController::class, 'index'])->name('admin')->middleware(['auth']);
+// Route::get('/admin', [DashboardController::class, 'index'])->name('admin')->middleware(['auth']);
 
 Route::resource('products', ProductController::class);
 
