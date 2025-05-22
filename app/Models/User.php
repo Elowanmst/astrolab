f21f61e293f3f788d671b846524116826ae80eb6
Cam- <veillardcamille01@gmail.com>
2025-05-22 09:58:28 +0200

    update user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,51 +2,51 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
-use Filament\Panel;
+// use Filament\Panel;
 
 class User extends Authenticatable implements FilamentUser
 {
-
+    
     use HasFactory, Notifiable;
-
+    
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    * The attributes that are mass assignable.
+    *
+    * @var list<string>
+    */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
-
-
-        public function canAccessPanel(Panel $panel): bool
-    {
-        return str_ends_with($this->email, '@astrolab.com') && $this->hasVerifiedEmail();
-    }
-
-
+    
+    //Décommenter cette fonction en prod pour s'assurer que seul un utilisateur avec un email @astrolab.com puisse accéder au panneau d'administration
+    // public function canAccessPanel(Panel $panel): bool
+    // {
+    //     return str_ends_with($this->email, '@astrolab.com') && $this->hasVerifiedEmail();
+    // }
+    
+    
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    * The attributes that should be hidden for serialization.
+    *
+    * @var list<string>
+    */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
+    
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    * Get the attributes that should be cast.
+    *
+    * @return array<string, string>
+    */
     protected function casts(): array
     {
         return [
